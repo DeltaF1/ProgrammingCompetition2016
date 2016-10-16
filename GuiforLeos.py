e1678f6629b2cf991e4fcd4449aed4eb1615fd9c
TheRealHex88 <tcushioncmail@gmail.com>
2016-10-15 21:16:58 -0400

    A button

--- GuiforLeos.py
+++ GuiforLeos.py
@@ -4,6 +4,11 @@ from tkinter import *
 root = Tk()
 root.title("Leo's Grocery Store Planner")
 
+def callback():
+    print("click")
+
+b = Button(root, text="export", command=callback)
+b.pack()
 
 
 root.mainloop()
