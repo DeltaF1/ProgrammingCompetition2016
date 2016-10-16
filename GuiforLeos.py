83e8788ab14f01d734ab50b9e7f3f4c835d2c037
TheRealHex88 <tcushioncmail@gmail.com>
2016-10-15 23:59:27 -0400

    The working notebook code

--- GuiforLeos.py
+++ GuiforLeos.py
@@ -1,31 +1,45 @@
+from tkinter import ttk
 from tkinter import *
 
 
 root = Tk()
 root.title("Leo's Grocery Store Planner")
 
-def stock_display():
-    print("Shows stock manager")
+frame_root = Frame(width=760, height=680)
+frame_root.grid()
 
+
+def stock_management_window():
+    
+    b = Button(stock_frame, text="export", command=export_doc)
+    b.place(x=660,y=600,width=100,height=25)
+
+        
 def work_schedule():
-    print("displays work schedule")
+    
+    b = Button(schedule_frame, text="export", command=export_doc)
+    b.place(x=660,y=600,width=100,height=25)
     
 
 def export_doc():
     print("nothing to export")
 
-frame = Frame(width=760, height=680)
-frame.grid()
 
-b_stocking = Button(root, text="Stock management", command=stock_display)
-b_stocking.place(x=0,y=0,width=150,height=25)
+n = ttk.Notebook(root)
+n.place(x=0,y=0,width=760, height=680)
+f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
+f2 = ttk.Frame(n)   # second page
+n.add(f1, text='Stock Manager')
+b = Button(f1, text="export", command=export_doc)
+b.place(x=660,y=600,width=100,height=25)
 
-b_shift = Button(root, text="Shift Schedule", command=work_schedule)
-b_shift.place(x=150,y=0,width=100,height=25)
+n.add(f2, text='Volunteer Schedule Manager')
+b = Button(f2, text="export", command=export_doc)
+b.place(x=660,y=600,width=100,height=25)
 
 
-b = Button(root, text="export", command=export_doc)
-b.place(x=660,y=655,width=100,height=25)
+
+
 
 
 
